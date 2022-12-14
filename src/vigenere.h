f8ce09d90e6972c0a093308e04e70b7d59bd4869
nicolasviste <nicolasviste@gmail.com>
2022-12-14 01:03:17 -0800

    Added encryption and storage of inputted phrases/keys

--- src/vigenere.h
+++ src/vigenere.h
@@ -3,13 +3,20 @@
 #define VIGENERE_H
 
 #include <iostream>
+#include <iomanip>
 #include <map>
 #include <string>
 #include <iterator>
+#include <cctype>
 
 using namespace std;
 
 void vigenere();
-void displayPhrases(std::map<string, string>);
+void displayPhrases();
+void menu();
+void encrypt();
+void decrypt();
+bool validString(string input);
+string toUpper(string input);
 
 #endif /* VIGENERE_H */
